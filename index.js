0c13cc1417db8233b2f921b9b7dc56d3297d9340
asramos98 <arnold.sleyder@gmail.com>
2023-05-31 11:40:06 -0500

    adicion para desencriptar un texto

--- index.js
+++ index.js
@@ -23,3 +23,30 @@ function encriptar(){
         parrafo.textContent = "Ingrese texto que desea encriptar o desencriptar";
     }
 }
+
+function desencriptar(){
+
+    let texto = document.getElementById("texto").value;
+    let imagen = document.getElementById("imagen");
+    let mensaje = document.getElementById("titulo-mensaje");
+    let parrafo = document.getElementById("parrafo");
+
+        let cifrado = texto
+        .replace(/enter/gi, "e")
+        .replace(/imes/gi, "i")
+        .replace(/ai/gi, "a")
+        .replace(/ober/gi, "o")
+        .replace(/ufat/gi, "u");
+
+    if(texto.length !=0){
+        document.getElementById("texto").value = cifrado;
+        imagen.src = "./img/open-g2121a8cb3_640.png";
+        mensaje.textContent = "Mensaje desencriptado";
+        parrafo.textContent = "";
+    }else{
+        alert("Ingrese un texto");
+        imagen.src = "./img/detective-g37b759611_640.png";
+        mensaje.textContent = "Ningun mensaje encontrado";
+        parrafo.textContent = "Ingrese texto que desea encriptar o desencriptar";
+    }
+}
